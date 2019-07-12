f37973c7a85fa05002b218802fd9d712b371e482
xiuer <1833549348@qq.com>
2019-07-12 21:44:37 +0800

    把本地工程添加到git

---
+++ git_test/git_test01/src/com/itheima/git/Demo01.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
